de2cbd8c8a9f07dfe737bbe52bf377cee3bed2c5
Katy Morris <katydmorris@gmail.com>
2023-01-29 18:03:45 -0700

    fix auth call

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
-require('dotenv').config({path: './.env'});
+const path = require('path');
+const dotenvPath = path.join(__dirname, '.env');
+require('dotenv').config({path: dotenvPath});
 const qs = require('qs');
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URI = process.env.REDIRECT_URI;
+const request = require('request');
 
 /* Create an HTTP server to handle responses */
 
@@ -12,40 +15,61 @@ var app = express();
 
 const port = 8888;
 
-function getAuthToken() {
-  const axios = require('axios');
-
-  const clientId = 'YOUR_CLIENT_ID';
-  const clientSecret = 'YOUR_CLIENT_SECRET';
-  const redirectUri = 'YOUR_REDIRECT_URI';
-
-  //conver CLIENT_ID and CLIENT_SECRET to base64
-  const base64 = Buffer.from(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64');
+async function getAuthToken() {
+  return new Promise((resolve, reject) => {
+    var client_id = CLIENT_ID;
+    var client_secret = CLIENT_SECRET;
   
-  const authOptions = {
-    url: 'https://accounts.spotify.com/api/token',
-    headers: {
-      'Authorization': 'Basic ' + base64,
-      'Content-Type': 'application/json'
-    },
-    form: qs.stringify({
-      grant_type: 'authorization_code',
-      code: 'AUTHORIZATION_CODE',
-      redirect_uri: REDIRECT_URI
-    }),
-    json: true
-  };
+    var authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      headers: {
+        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')),
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      body: {
+        grant_type: 'refresh_token',
+      },
+      json: true
+    };
   
-  axios.post(authOptions.url, authOptions)
-    .then(response => {
-      console.log(response.data)
-    })
-    .catch(error => {
-      console.log(error)
-    });  
+    request.post(authOptions, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+        resolve(response.body.access_token)
+      } else {
+        console.log('error: ' + error);
+        console.log('response.statusCode: ' + response.statusCode);
+        console.log('response.statusText: ' + response.statusText);
+        reject(error)
+      }
+    });
+  })
+  
 }
+
+function getCurrentSong(token) {
+  var options = {
+    'method': 'GET',
+    'url': 'https://api.spotify.com/v1/me/player/currently-playing',
+    'headers': {
+      'content': 'application/json',
+      'Authorization': 'Bearer ' + token,
+    }
+  };
+  request(options, function (error, response) {
+    if (error) throw new Error(error);
+    console.log(response.body);
+  });
+}
+
 
 app.listen(port, () => {
   console.log(`example app listening at http://localhost:${port}`)
-  getAuthToken();
+  // promise for getting auth token
+  getAuthToken()
+    .then((token) => {
+      console.log('token: ' + token);
+      getCurrentSong(token);
+    }).catch((err) => {
+      console.log('error: ' + err);
+    })
 })
